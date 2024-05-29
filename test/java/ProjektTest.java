dc8beb92e5c1c97f56af9ed6c0427b151197a364
not me <92511354+notkulonme@users.noreply.github.com>
2024-05-29 20:24:17 +0200

    Add files via upload

--- test/java/ProjektTest.java
+++ test/java/ProjektTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +13,7 @@ class ProjektTest {
         var expected = new int[]{1, 1, 1, 1, 1};
         assertArrayEquals(expected, projekt.getDiagonal(matrix, 0, 0, 'r'));
     }
+
     @Test
     void testGetDiagnalLeft() {
         var projekt = new Projekt();
@@ -56,6 +58,18 @@ class ProjektTest {
         list.add(3);
         list.add(4);
         list.add(5);
-        assertEquals(list, projekt.matrixToList(new int[][]{{1}, {2}, {3}, {1,2,3,4}, {5} }));
+        assertEquals(list, projekt.matrixToList(new int[][]{{1}, {2}, {3}, {1, 2, 3, 4}, {5}}));
+    }
+
+    private int[][] testMatrix = {{1, 1, 1, 1, 1}, {2, 2, 2, 2, 2}, {3, 3, 3, 3, 3}, {4, 4, 4, 4, 4}, {5, 5, 5, 5, 5}};
+
+    @Test
+    void sumUnderMainDiagonal_test_with_testMatrix() {
+        assertEquals(2 + 6 + 3 * 4 + 4 * 5, Projekt.sumUnderMainDiagonal(testMatrix));
+    }
+
+    @Test
+    void findAllPrime_Test_With_testMatrix() {
+        assertEquals(new ArrayList<Integer>(List.of(2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 5, 5, 5, 5, 5)), Projekt.findAllPrime(Projekt.matrixToList(testMatrix)));
     }
 }
